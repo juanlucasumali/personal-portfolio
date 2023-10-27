2f4f3c316c2650bc642da7b96778fd191073e376
Juan Lucas Umali <juanlucasumali@gmail.com>
2023-10-26 19:48:23 -0700

    experiences section

---
+++ src/components/resume/Data.jsx
@@ -0,0 +1,32 @@
+const Data = [
+    {
+        id: 1,
+        icon: "icon-graduation",
+        year: "2019 - present",
+        title: "Academic Degree",
+        desc: "...",
+    },
+    {
+        id: 2,
+        icon: "icon-briefcase",
+        year: "2019 - present",
+        title: "Academic Degree",
+        desc: "...",
+    },
+    {
+        id: 3,
+        icon: "icon-briefcase",
+        year: "2019 - present",
+        title: "Academic Degree",
+        desc: "...",
+    },
+    {
+        id: 4,
+        icon: "icon-briefcase",
+        year: "2019 - present",
+        title: "Academic Degree",
+        desc: "...",
+    },
+]
+
+export default Data
--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import "./resume.css"
+import Data from './Data';
+import Card from './Card';
 
 const Resume = () => {
     return (
-        <div>Resume</div>
+        <section className="resume container section" id="resume">
+            <h2 className="section__title">experience</h2>
+                <div className="timeline grid">
+                    {Data.map((val, id) => {
+                        return (
+                            <Card 
+                                key={id} 
+                                icon={val.icon} 
+                                title={val.title}
+                                year={val.year} 
+                                desc={val.desc}
+                            />
+                        )
+                    })}
+                </div>
+        </section>
     )
 }
 
