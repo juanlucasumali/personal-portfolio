a56ac6d57a133145bbb7ead3ae6f2acd137f4d36
Juan Lucas Umali <juanlucasumali@gmail.com>
2023-10-27 01:22:31 -0700

    completed portfolio skeleton

---
+++ src/components/portfolio/Menu.jsx
@@ -0,0 +1,33 @@
+import Work1 from "../../assets/work-1.svg";
+import Work2 from "../../assets/work-2.svg";
+import Work3 from "../../assets/work-3.svg";
+import Work4 from "../../assets/work-4.svg";
+
+const Menu = [
+    {
+        id: 1,
+        image: Work1,
+        title: "software title",
+        category: "software"
+    },
+    {
+        id: 2,
+        image: Work2,
+        title: "music title",
+        category: "music"
+    },
+    {
+        id: 3,
+        image: Work3,
+        title: "dance title",
+        category: "dance"
+    },
+    {
+        id: 4,
+        image: Work4,
+        title: "lit title",
+        category: "literature"
+    },
+]
+
+export default Menu
--- src/components/uno/UnoAbout.jsx
+++ src/components/uno/UnoAbout.jsx
@@ -1,18 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
 const UnoAbout = () => {
-
-    const [animationStage, setAnimationStage] = useState(0);
-
-    // Toggle between the two mouth shapes
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setAnimationStage(prev => (prev + 1) % 2); // cycle between 0 and 1
-        }, 200); // adjust this time for desired animation speed
-
-        return () => clearInterval(interval);
-    }, []);
-
     return (
         <svg 
             viewBox="-5 -5 46 46" 
@@ -24,10 +12,7 @@ const UnoAbout = () => {
         >
             <rect x="0" y="0" width="36" height="36" fill="#f97855" rx="36"></rect>
             <g transform="translate(-7 4.5) rotate(9 50 90)">
-                {animationStage === 0 ? 
-                    <path d="M13,18 a01,0.95 0 0,0 10,0" fill="#000000"></path> :
-                    <path d="M15 18c2 1 4 1 6 0" stroke="#000000" fill="none" strokeLinecap="round"></path>
-                }
+                <path d="M13,18 a01,0.95 0 0,0 10,0" fill="#000000"></path>
                 <rect x="10" y="14" width="1.5" height="2" rx="1" stroke="none" fill="#000000"></rect>
                 <rect x="24" y="14" width="1.5" height="2" rx="1" stroke="none" fill="#000000"></rect>
             </g>
