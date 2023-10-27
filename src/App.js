8d6715aabd5585e3291e387b34ee94d55e74eea7
Juan Lucas Umali <juanlucasumali@gmail.com>
2023-10-27 11:24:49 -0700

    finished contact portion

--- src/App.js
+++ src/App.js
@@ -6,9 +6,6 @@ import About from './components/about/About';
 import Services from './components/services/Services';
 import Resume from './components/resume/Resume';
 import Portfolio from './components/portfolio/Portfolio';
-import Pricing from './components/pricing/Pricing';
-import Testimonials from './components/testimonials/Testimonials';
-import Blog from './components/blog/Blog';
 import Contact from './components/contact/Contact';
 
 
@@ -22,9 +19,6 @@ const App = () => {
       <Services />
       <Resume />
       <Portfolio />
-      <Pricing />
-      <Testimonials />
-      <Blog />
       <Contact />
     </main>
     </>
